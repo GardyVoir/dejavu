63c1614d4f42cdc34d9e32c4c8c81ae7ddf3e112
Nathan DIERICKX <nath59310@gmail.com>
2022-05-19 11:26:08 +0200

    Updated API Request

--- lib/api/apiTMDB.dart
+++ lib/api/apiTMDB.dart
@@ -1,7 +1,18 @@
 class apiTMDB {
-  var api_key = "&api_key=151dfa1b4c6a83a02970c0c6612615b3";
+    static const BASE_URL = "https://api.themoviedb.org/3/",
+    IMAGE_URL = "http://image.tmdb.org/t/p/",
+    API_KEY = "?api_key=151dfa1b4c6a83a02970c0c6612615b3",
+    SEARCH_QUERY = "search/movie?query=";
+  
+    static getMovieWithSearch(String query) {
+        return BASE_URL + SEARCH_QUERY + query + API_KEY;
+    }
 
-    getMovieWithSearch(String query) {
-        return "https://api.themoviedb.org/3/search/movie?query="+ query + api_key;
+    static getMovieDetail(String id) {
+        return BASE_URL + 'movie/' + id + API_KEY;
+    }
+
+    static getMovieImg(String imgPath) {
+        return IMAGE_URL + imgPath;
     }
 }
